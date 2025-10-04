2c11095fc63a771a4f219cb0c607712b0e8ad4f6
zakariaOuakrim <zakaria.ouakrim@marketingconfort.com>
2025-10-04 21:49:58 +0100

    initial commit

---
+++ src/main/java/com/medical/kafkaspringstreams/KafkaSpringStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.medical.kafkaspringstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringStreamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medical/kafkaspringstreams/KafkaSpringStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.kafkaspringstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

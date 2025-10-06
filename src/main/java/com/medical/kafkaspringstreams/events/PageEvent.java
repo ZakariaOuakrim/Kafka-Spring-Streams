9becba42864f807f1410cfaa727d562d3dddc89f
= <=>
2025-10-06 19:50:49 +0100

    real time with kafka streams added

--- src/main/java/com/medical/kafkaspringstreams/events/PageEvent.java
+++ src/main/java/com/medical/kafkaspringstreams/events/PageEvent.java
@@ -2,5 +2,5 @@ package com.medical.kafkaspringstreams.events;
 
 import java.util.Date;
 
-public record PageEvent(String user, String page, Date date,long duration) {
+public record PageEvent(String name, String user, Date date,long duration) {
 }
